22b5af9ceb9357104273ffbf9c6eaf0fbe9156c8
firo <samuraitorikku@yahoo.co.jp>
2020-02-05 10:39:47 +0900

    Add Report class

---
+++ src/models/Report.java
@@ -0,0 +1,126 @@
+package models;
+
+import java.sql.Date;
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+// RDBのテーブル名の指定。
+@Table(name = "reports")
+//DB Query
+@NamedQueries({
+    @NamedQuery(
+            name = "getAllReports",
+            query = "SELECT r FROM Report AS r ORDER BY r.id DESC"
+            ),
+    @NamedQuery(
+            name = "getReportsCount",
+            query = "SELECT COUNT(r) FROM Report AS r"
+            ),
+})
+// Entity : 抽象的なモデル化
+@Entity
+public class Report {
+    //主キーの自動生成を指定する。
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    //社員番号 外部と繋げる
+    @ManyToOne
+    @JoinColumn(name = "employee_id", nullable = false)
+    private Employee employee;
+
+    //レポートデータ Date:年月日を管理
+    @Column(name = "report_date", nullable = false)
+    private Date report_date;
+
+    // タイトル
+    @Column(name = "title", length=255, nullable = false)
+    private String title;
+
+    // 詳細内容 @Lob : text改行情報も保持
+    @Lob
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    //作成日
+    @Column(name = "created_at", nullable = false)
+    private Timestamp created_at;
+
+    //更新日
+    @Column(name = "updated_at", nullable = false)
+    private Timestamp updated_at;
+
+    //ゲッターセッターの追加
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    public Date getReport_date() {
+        return report_date;
+    }
+
+    public void setReport_date(Date report_date) {
+        this.report_date = report_date;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Timestamp getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Timestamp created_at) {
+        this.created_at = created_at;
+    }
+
+    public Timestamp getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(Timestamp updated_at) {
+        this.updated_at = updated_at;
+    }
+
+
+
+}
